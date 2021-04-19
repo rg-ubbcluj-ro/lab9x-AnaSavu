ecb8642753e2fdce270fb9c3c248a2b283893eb5
AnaSavu <anasavu001@>
2021-04-20 00:42:29 +0300

    added bke component in webapp

--- bikes-web/src/main/webapp/src/app/app-routing.module.ts
+++ bikes-web/src/main/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BikesComponent} from "./bikes/bikes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bikes', component: BikesComponent},
+  // {path 'bike/detail/:id', component: BikeDe}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
