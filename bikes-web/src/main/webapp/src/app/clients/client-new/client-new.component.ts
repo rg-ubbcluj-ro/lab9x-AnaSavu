c215ac6014e804b0b27abe33c95961d6faaa1080
AnaSavu <anasavu001@>
2021-04-29 17:37:14 +0300

    added reactive forms on client

--- bikes-web/src/main/webapp/src/app/clients/client-new/client-new.component.ts
+++ bikes-web/src/main/webapp/src/app/clients/client-new/client-new.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Location} from "@angular/common";
 import {ClientService} from "../shared/client.service";
 import {Client} from "../shared/client.model";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-client-new',
@@ -9,22 +10,38 @@ import {Client} from "../shared/client.model";
   styleUrls: ['./client-new.component.css']
 })
 export class ClientNewComponent implements OnInit {
+  // clientForm: FormGroup;
+  clientForm = this.fb.group({
+    name: ['', Validators.required],
+    age: ['', Validators.required],
+    phoneNumber: ['', Validators.required]
+  });
 
-  constructor(private clientService: ClientService, private location: Location) { }
+  constructor(private clientService: ClientService, private location: Location, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
   }
 
-  saveClient(name: string, age: string, phoneNumber: string) {
+  // saveClient(name: string, age: string, phoneNumber: string) {
+  //   const client: Client = <Client>{
+  //     name,
+  //     age: +age,
+  //     phoneNumber
+  //   };
+  //   this.clientService.saveClient(client).subscribe(c1 => console.log('saved client: ', c1));
+  // }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  submitClient(clientForm: FormGroup): void {
     const client: Client = <Client>{
-      name,
-      age: +age,
-      phoneNumber
+      name: clientForm.value.name,
+      age: +clientForm.value.age,
+      phoneNumber: clientForm.value.phoneNumber
     };
     this.clientService.saveClient(client).subscribe(c1 => console.log('saved client: ', c1));
-  }
-
-  goBack() {
-    this.location.back();
   }
 }
